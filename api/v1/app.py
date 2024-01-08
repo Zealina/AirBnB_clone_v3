b4787ffc736ec1eebe599252f4953c616011c7fb
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-01-08 18:04:51 +0000

    Error Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Main Flask Endpoint"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -15,6 +15,12 @@ app.register_blueprint(app_views)
 def teardown_appcontext(exception=None):
     """Handle teardown appcontext"""
     storage.close()
+
+
+@app.errorhandler(404)
+def error_not_found(error):
+    """Error not found"""
+    return jsonify({'error': 'Not found'})
 
 
 if __name__ == '__main__':
